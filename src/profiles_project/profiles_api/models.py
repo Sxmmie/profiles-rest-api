658fed7ca9f9923fd0590d121d1cbfecca9b4bc6
Sxmmie <samuelumohjnr@gmail..com>
2019-07-10 15:18:13 +0100

    Added model for storing profile feed items, created migration

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -80,3 +80,17 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         objects as string, convert objects to string
         """
         return self.email
+
+
+class ProfileFeedItem(models.Model):
+    """Profile status  update"""
+
+    # field that points to the user profile this update corresponds to.
+    # Foreign key creates a link from a=ne model to another model in the database.
+    user_profile = models.ForeignKey("UserProfile", on_delete=models.CASCADE)
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Return the model as string"""
+        return self.status_text
